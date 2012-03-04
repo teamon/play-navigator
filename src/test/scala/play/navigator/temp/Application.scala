461f569c7cb65b3c6a28a790880b573978f5ad3d
Tymon Tobolski <i@teamon.eu>
2012-03-04 02:14:22 +0100

    commend old code

--- src/test/scala/play/navigator/temp/Application.scala
+++ src/test/scala/play/navigator/temp/Application.scala
@@ -1,41 +1,41 @@
-package controllers
+// package controllers
 
-import play.api.mvc._
+// import play.api.mvc._
 
-object Application extends Controller {
+// object Application extends Controller {
 
-  def index(): Action[_] = Action {
-    Ok("Applcation.index => " + routes.index())
-  }
+//   def index(): Action[_] = Action {
+//     Ok("Applcation.index => " + routes.index())
+//   }
 
-  def about(): Action[_] = Action {
-    Ok("Application.about => " + routes.about() + " or " + routes.api.v2.about())
-  }
+//   def about(): Action[_] = Action {
+//     Ok("Application.about => " + routes.about() + " or " + routes.api.v2.about())
+//   }
 
-  def show(id: Int): Action[_] = Action {
-    Ok("Application.show(%d) => %s" format (id, routes.show(id)))
-  }
+//   def show(id: Int): Action[_] = Action {
+//     Ok("Application.show(%d) => %s" format (id, routes.show(id)))
+//   }
 
-  def bar(f: Float, b: Boolean, s: String): Action[_] = Action {
-    Ok("Application.bar(%f, %b, %s) => %s" format (f, b, s, routes.bar(f,b,s)))
-  }
+//   def bar(f: Float, b: Boolean, s: String): Action[_] = Action {
+//     Ok("Application.bar(%f, %b, %s) => %s" format (f, b, s, routes.bar(f,b,s)))
+//   }
 
-  def long(path: String) = Action {
-    Ok("Application.long(%s)" format path)
-  }
+//   def long(path: String) = Action {
+//     Ok("Application.long(%s)" format path)
+//   }
 
-  def extJson(id: Int) = Action { Ok("Application.extJson(%d)" format id) }
-  def extXml(id: String) = Action { Ok("Application.extXml(%s)" format id) }
+//   def extJson(id: Int) = Action { Ok("Application.extJson(%d)" format id) }
+//   def extXml(id: String) = Action { Ok("Application.extXml(%s)" format id) }
 
-  import play.api.libs.iteratee._
+//   import play.api.libs.iteratee._
 
-  def ws() = WebSocket.using[String] { request =>
-    val in = Iteratee.foreach[String](println).mapDone { _ =>
-      println("Disconnected")
-    }
+//   def ws() = WebSocket.using[String] { request =>
+//     val in = Iteratee.foreach[String](println).mapDone { _ =>
+//       println("Disconnected")
+//     }
 
-    val out = Enumerator("Hello!")
+//     val out = Enumerator("Hello!")
 
-    (in, out)
-  }
-}
+//     (in, out)
+//   }
+// }
